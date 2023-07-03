9a1e8d5ad5eca213948b9c54c3021ffe17e1f7fa
Hoa <Hoa@Bong>
2023-07-03 22:07:18 +0700

    Create new repository

---
+++ testcases/com/nopcommerce/users/Level_01_Register.java
@@ -0,0 +1,23 @@
+package com.nopcommerce.users;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Level_01_Register {
+  
+  @BeforeClass
+  public void beforeClass() {
+  }
+  @Test
+  public void Register_1_EmtyData() {
+  }
+  
+  @Test
+  public void Register_2_EmailInvalid() {
+  }
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
